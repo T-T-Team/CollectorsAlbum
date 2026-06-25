6906f9dcba77d7d5b7ed29043e04cd9a0bbcabbb
Toma <novotny.tom96@gmail.com>
2026-06-25 11:06:26 +0200

    Active album label

--- common/src/main/java/team/tnt/collectorsalbum/common/item/AlbumItem.java
+++ common/src/main/java/team/tnt/collectorsalbum/common/item/AlbumItem.java
@@ -11,12 +11,19 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.level.Level;
 import team.tnt.collectorsalbum.client.CollectorsAlbumClient;
+import team.tnt.collectorsalbum.common.Album;
+import team.tnt.collectorsalbum.common.tracking.PlayerAlbumTracker;
+import team.tnt.collectorsalbum.platform.Platform;
+import team.tnt.collectorsalbum.platform.Side;
 
+import java.util.UUID;
 import java.util.function.Consumer;
 
 public class AlbumItem extends Item {
 
     public static final Component USAGE = Component.translatable("collectorsalbum.label.press_open").withStyle(ChatFormatting.GRAY);
+    public static final Component ALBUM_ACTIVE = Component.translatable("collectorsalbum.label.album_active").withStyle(ChatFormatting.GREEN, ChatFormatting.ITALIC);
+    public static final Component ALBUM_INACTIVE = Component.translatable("collectorsalbum.label.album_inactive").withStyle(ChatFormatting.RED, ChatFormatting.ITALIC);
 
     public AlbumItem(Item.Properties properties) {
         super(properties);
@@ -34,5 +41,18 @@ public class AlbumItem extends Item {
     @Override
     public void appendHoverText(ItemStack itemStack, TooltipContext context, TooltipDisplay display, Consumer<Component> builder, TooltipFlag tooltipFlag) {
         builder.accept(USAGE);
+        Album album = Album.fromItem(itemStack);
+        if (album == null)
+            return;
+        PlayerAlbumTracker tracker = PlayerAlbumTracker.get();
+        if (Platform.INSTANCE.getSide() == Side.CLIENT) {
+            UUID clientId = CollectorsAlbumClient.getClientUUID();
+            if (clientId == null)
+                return;
+            Component label = tracker.matches(clientId, album)
+                    ? ALBUM_ACTIVE
+                    : ALBUM_INACTIVE;
+            builder.accept(label);
+        }
     }
 }
